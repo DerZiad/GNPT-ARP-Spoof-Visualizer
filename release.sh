cf02cf6e960bb9665db9eb3c924e0db68005845d
Ziad Bougrine <40337316+DerZiad@users.noreply.github.com>
2025-07-16 20:33:12 +0200

    31 Fix-release task (#32)

--- release.sh
+++ release.sh
@@ -2,18 +2,15 @@
 
 set -e
 
-# Build the project
+java -version
 mvn clean compile assembly:single
 
-# Prepare release directory
 rm -rf gnpt
 mkdir gnpt
 
-# Move built jar and resources
 mv target/NetworkPacketTracer-1.0.jar-jar-with-dependencies.jar gnpt/NetworkPacketTracer.jar
 cp -r javafx/ gnpt/
 
-# Set permissions
 chmod -R 755 gnpt/
 
 echo "Network Packet Tracer has been built and is ready for release."
