11fda804ed26a99cf295c906bd07d3de151b8210
Ziad Bougrine <40337316+DerZiad@users.noreply.github.com>
2026-01-04 10:02:56 +0100

    49 remove unnecessary code (#50)

--- build.sh
+++ build.sh
@@ -2,20 +2,32 @@
 
 set -e
 
+YELLOW='\033[33m'
+RESET='\033[0m'
+RELEASE_JAR_NAME="gnpt.jar"
+
+log() {
+  echo -e "${YELLOW}[BASH]${RESET} $*"
+}
+
+log "Building GNPT Jar ..."
 java -version
 mvn clean compile assembly:single
+mv target/gnpt-1.0-jar-with-dependencies.jar target/$RELEASE_JAR_NAME
+setopt extendedglob
+rm -rf target/^$RELEASE_JAR_NAME
+cp -r javafx target
+chmod -R 755 target
 
-rm -rf gnpt
-mkdir gnpt
+log "GNPT has been built and is ready for release."
 
-mv target/NetworkPacketTracer-1.0.jar-jar-with-dependencies.jar gnpt/NetworkPacketTracer.jar
-cp -r javafx/ gnpt/
-
-chmod -R 755 gnpt/
-
-echo "Network Packet Tracer has been built and is ready for release."
 echo "#!/usr/bin/zsh
-java --module-path ./javafx/lib --add-modules javafx.controls,javafx.fxml,javafx.swing -jar NetworkPacketTracer.jar
-" > gnpt/run.sh
+java --module-path ./javafx/lib --add-modules javafx.controls,javafx.fxml,javafx.swing -jar gnpt.jar
+" > target/run.sh
 
-zip -r gnpt.zip gnpt
+chmod +x target/run.sh
+
+log "Creating gnpt.zip..."
+cp -r target gnpt
+zip -r target/gnpt.zip gnpt
+rm -rf gnpt
